b99b635690cb11307ae5c03277d3eab3bc68d29f
Jose manuel <150991059+mikerodriguez1@users.noreply.github.com>
2026-02-09 04:40:48 -0600

    Update main.cpp

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     string m1, mod1, c1, m2, mod2, c2;
 
-    // Pedimos datos de los dos equipos
+    
     cout << "Configurando Celular 1..." << endl;
     cout << "Marca: "; getline(cin, m1);
     cout << "Modelo: "; getline(cin, mod1);
@@ -22,17 +22,17 @@ int main() {
 
     cout << "\n--- Iniciando pruebas ---\n" << endl;
 
-    // Pruebas Objeto 1
+    
     cout << "Estado inicial del celular 1:" << endl;
     c1_obj.mostrarEstado();
     c1_obj.encender();
     c1_obj.usar(20);
     c1_obj.mostrarEstado();
 
-    // Pruebas Objeto 2
+    
     cout << "Estado del celular 2:" << endl;
     c2_obj.encender();
-    c2_obj.cargar(60); // Intento de sobrecarga
+    c2_obj.cargar(60); 
     c2_obj.usar(15);
     c2_obj.mostrarEstado();
 
