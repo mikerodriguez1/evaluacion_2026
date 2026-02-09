09a66a4e465c3c0d4104604c0f68cb63e33edfa4
Jose manuel <150991059+mikerodriguez1@users.noreply.github.com>
2026-02-09 04:39:50 -0600

    Update ClaseObjeto.h

--- Proyecto/ClaseObjeto.h
+++ Proyecto/ClaseObjeto.h
@@ -6,16 +6,16 @@ using namespace std;
 
 class Celular {
 private:
-    // Atributos privados para que no los modifiquen desde fuera
+    
     string marca, modelo, color;
     int bateria;
     bool encendido;
 
 public:
-    // Constructor para inicializar el equipo
+    
     Celular(string m, string mod, string c);
 
-    // Acciones principales
+    
     void encender();
     void apagar();
     void cargar(int cantidad);
